727c1a84ee7cddfdb2e9cfe561f290a2f0178cd1
LucCntt4 <119579729+LucCntt4@users.noreply.github.com>
2024-03-01 09:17:30 +0700

    Add files via upload

---
+++ project23/user.php
@@ -0,0 +1,19 @@
+<?php
+$users = [
+    [
+    "username" => "johndoe",
+    "password" => password_hash("password123", PASSWORD_DEFAULT), // Store hashed password
+    "name" => "John Doe",
+    "email" => "johndoe@example.com"
+    ],
+    // Add more users...
+   ];
+   $users = [
+    [
+    "username" => "abc",
+    "password" => password_hash("123", PASSWORD_DEFAULT), // Store hashed password
+    "name" => "John Doe",
+    "email" => "johndoe@example.com"
+    ],
+   ];
+?>
